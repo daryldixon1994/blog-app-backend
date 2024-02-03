40b4c8d5304518a97f4bd75852612a5067d30769
daryldixon1994 <kmohamedanouar@gmail.com>
2024-02-03 16:46:24 +0100

    update user add like

--- routes/user/addLike.js
+++ routes/user/addLike.js
@@ -3,14 +3,21 @@ const Blog = require("../../models/Blog");
 module.exports = async (req, res) => {
   try {
     let { id, blogId } = req.query;
-    const likeAdded = await Blog.findOneAndUpdate(
-      { _id: blogId },
-      {
-        $push: { likes: id },
-      },
-      { new: true }
-    );
-    res.status(200).json({ status: true, data: likeAdded });
+    let blog = await Blog.findById(blogId);
+    if (!blog.likes.includes(id)) {
+      const likeAdded = await Blog.findOneAndUpdate(
+        { _id: blogId },
+        {
+          $push: { likes: id },
+        },
+        { new: true }
+      );
+      return res.status(200).json({ status: true, data: likeAdded });
+    } else {
+      return res
+        .status(200)
+        .json({ status: true, data: "Like has been already added" });
+    }
   } catch (error) {
     if (error) throw error;
     res.status(401).json({ status: false, error });
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 require("dotenv").config();
-const cors = require('cors')
+const cors = require("cors");
 
 //ENVIRONMENT VARIABLES
 const DB = process.env.DB;
